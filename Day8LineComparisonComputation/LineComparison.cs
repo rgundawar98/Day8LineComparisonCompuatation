be8ab66e1b96e7bf18ec540b0ba2f2071907aac0
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-17 17:19:44 +0530

    Checking which length is greater UC2_CompareTwoLength

--- Day8LineComparisonComputation/LineComparison.cs
+++ Day8LineComparisonComputation/LineComparison.cs
@@ -10,7 +10,7 @@ namespace Day8LineComparisonComputation
     {
         public void Check_Lines()
         { 
-            double lengthx=0 ,lengthy=0, LineLength = 0;
+            double lengthx=0 ,lengthy=0, LineLength = 0 , lengthA=0 ,lengthB=0,LineLength1=0;
             Console.WriteLine("Enter the Cartesian points for valx1 and valx2");
             int valx1 = Convert.ToInt32(Console.ReadLine());
             int valx2 = Convert.ToInt32(Console.ReadLine());
@@ -23,6 +23,32 @@ namespace Day8LineComparisonComputation
             Console.WriteLine("The length of cartesian points length is lengthy:" + lengthy);
             LineLength = Math.Sqrt(lengthx *lengthy);
             Console.WriteLine("The length of the line is:"+LineLength);
+
+            Console.WriteLine("Enter the Cartesian points for valA1 and valA2");
+            int valA1 = Convert.ToInt32(Console.ReadLine());
+            int valA2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Cartesian points for valB1 and valB2");
+            int valB1 = Convert.ToInt32(Console.ReadLine());
+            int valB2 = Convert.ToInt32(Console.ReadLine());
+            lengthA = Math.Pow(valA2 - valA1, 2);
+            lengthB = Math.Pow((valB2 - valB1), 2);
+            Console.WriteLine("The length of cartesian points length is lengthx:" + lengthA);
+            Console.WriteLine("The length of cartesian points length is lengthy:" + lengthB);
+            LineLength1 = Math.Sqrt(lengthA * lengthB);
+            Console.WriteLine("The length of the line is:" + LineLength1);
+
+            if(LineLength == LineLength1)
+            {
+                Console.WriteLine("Both lines are equal to each other");
+            }
+            else if(LineLength > LineLength1)
+            {
+                Console.WriteLine("First length is greater than second");
+            }
+            else
+            {
+                Console.WriteLine("Both lines are not similar");
+            }
         }
     }
 }
